c827b01a52ed83ab95ff2d1999b6bed82f120e72
Meowlynna <vannestheo@gmail.com>
2025-02-24 13:38:40 +0700

    ketinggalan

--- public/js/custom-script.js
+++ public/js/custom-script.js
@@ -14,13 +14,6 @@ function hideLoadingPopup() {
 }
 
 $(document).ready(() => {
-	// Prevent form from submitting when enter key presses
-	$('input[type="text"]').on("keydown", function(event) {
-		if (event.key === "Enter") {
-			event.preventDefault();
-		}
-	});
-
 	// Show loading popup on form submission
 	$('form').on('submit', function () {
 		showLoadingPopupWithDelay();
--- resources/views/roles/admin/course/index.blade.php
+++ resources/views/roles/admin/course/index.blade.php
@@ -82,7 +82,7 @@
 						</tr>
 					@empty
 						<tr class="bg-white">
-							<td class="py-2 px-4 text-center" colspan="5">- No data found -</td>
+							<td class="py-2 px-4 text-center" colspan="7">- No data found -</td>
 						</tr>
 					@endforelse
 				</tbody>
