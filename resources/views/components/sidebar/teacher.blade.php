7cb5abdc73790b0cd8febb611cf8024eddb65999
VTS <vannestheo@gmail.com>
2025-10-01 17:28:25 +0700

    hehehehehehhehe

--- resources/views/components/sidebar/teacher.blade.php
+++ resources/views/components/sidebar/teacher.blade.php
@@ -36,22 +36,31 @@
 
 		{{-- Sidebar navigations --}}
 		<div class="flex flex-col items-stretch justify-center w-full text-base" id="large-sidebar">
-			<x-anchor-button class="grow flex items-center text-start gap-4 py-3 px-6"
+			<x-anchor-button class="w-full grow flex justify-between items-center text-start gap-4 py-3 px-6"
 				href="{{ route('home') }}" style="transform: scale(1); border-radius: 0; background: {{ Request::is('dashboard*')? 'linear-gradient(90deg, #1EB8CD 31%, rgba(53, 77, 155, 0) 100%)' : '' }};">
-				<img src="{{ asset('img/sidebar-dashboard.svg') }}" class="h-6 w-8" alt="sidebar-icon"> Dashboard
+			    <div class="w-1/8">
+				    <img src="{{ asset('img/sidebar-dashboard.svg') }}" class="h-6 w-8" alt="sidebar-icon">
+				</div>
+				<div class="flex flex-col w-7/8"><h3>Dashboard</h3><span class="text-xs font-normal">Find out helpful widgets made special for helping you</span></div>
 			</x-anchor-button>
 
-			<x-anchor-button class="grow flex items-center text-start gap-4 py-3 px-6"
+			<x-anchor-button class="w-full grow flex justify-between items-center text-start gap-4 py-3 px-6"
 				href="{{ route('teacher.course.index') }}" style="transform: scale(1); border-radius: 0; background: {{ Request::is('teacher*course*')? 'linear-gradient(90deg, #1EB8CD 31%, rgba(53, 77, 155, 0) 100%)' : '' }};">
-				<img src="{{ asset('img/sidebar-courses.svg') }}" class="h-6 w-8" alt="sidebar-icon"> Courses
+			    <div class="w-1/8">
+				    <img src="{{ asset('img/sidebar-courses.svg') }}" class="h-6 w-8" alt="sidebar-icon">
+				</div>
+				<div class="flex flex-col w-7/8"><h3>Manage Courses</h3><span class="text-xs font-normal">Find out syllabus and your class info here</span></div>
 				@if(!session('all_course_has_topics'))
 					<div class="h-6 w-6 rounded-full bg-red absolute animate-bounce text-white flex items-center justify-center" style="top: 0; right: 0;">!</div>
 				@endif
 			</x-anchor-button>
 
-			<x-anchor-button class="grow flex items-center text-start gap-4 py-2 px-6"
+			<x-anchor-button class="w-full grow flex justify-between items-center text-start gap-4 py-2 px-6"
 				href="{{ route('teacher.student.select-course') }}" style="transform: scale(1); border-radius: 0; background: {{ Request::is('teacher*student*')? 'linear-gradient(90deg, #1EB8CD 31%, rgba(53, 77, 155, 0) 100%)' : '' }};">
-				<i class="bi bi-person-fill text-3xl mr-0.5"></i> Students
+				<div class="w-1/8">
+				    <i class="bi bi-person-fill text-3xl mr-0.5"></i>
+				</div>
+				<div class="flex flex-col w-7/8"><h3>Manage Students</h3><span class="text-xs font-normal">Find out each student's portfolio and assessment here</span></div>
 				@if(session('there_is_student_with_no_progress_unlocked'))
 					<div class="h-6 w-6 rounded-full bg-red absolute animate-bounce text-white flex items-center justify-center" style="top: 0; right: 0;">!</div>
 				@endif
@@ -62,14 +71,17 @@
 				<img src="{{ asset('img/sidebar-forum.svg') }}" class="h-6 w-8"> Forum Discussion
 			</x-anchor-button>
 
-			<x-anchor-button class="grow flex items-center text-start gap-4 py-3 px-6"
+			<x-anchor-button class="w-full grow flex items-center text-start gap-4 py-3 px-6"
 				href="{{ route('teacher.assignment.index') }}" style="transform: scale(1); border-radius: 0; background: {{ Request::is('teacher*assignment*')? 'linear-gradient(90deg, #1EB8CD 31%, rgba(53, 77, 155, 0) 100%)' : '' }};">
 				<img src="{{ asset('img/sidebar-assignment.svg') }}" class="h-6 w-8" alt="sidebar-icon"> Assignment
 			</x-anchor-button> --}}
 
-			<x-anchor-button class="grow flex items-center text-start gap-4 py-3 px-6"
+			<x-anchor-button class="grow flex justify-between items-center text-start gap-4 py-3 px-6"
 				href="{{ route('teacher.attendance.index') }}" style="transform: scale(1); border-radius: 0; background: {{ Request::is('teacher*attendance*')? 'linear-gradient(90deg, #1EB8CD 31%, rgba(53, 77, 155, 0) 100%)' : '' }};">
-				<img src="{{ asset('img/sidebar-attendance.svg') }}" class="h-6 w-8" alt="sidebar-icon"> Attendance
+				<div class="w-1/8">
+				    <img src="{{ asset('img/sidebar-attendance.svg') }}" class="h-6 w-8" alt="sidebar-icon">
+				</div>
+				<div class="flex flex-col w-7/8"><h3>Manage Attendance</h3><span class="text-xs font-normal">Find out you and your students attendance info here</span></div>
 			</x-anchor-button>
 
 			{{-- <x-anchor-button class="grow flex items-center text-start gap-4 py-2 px-6"
